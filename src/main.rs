990e454d431bdfb07aade2e1012a8601f280f3f7
Matt Kingston <mattkingston@protonmail.com>
2022-12-08 21:53:12 +0100

    Collect values into result type

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use xattr;
 use clap::Parser;
 use std::path::PathBuf;
 use std::str;
-use std::ffi::OsStr;
+use std::ffi::{OsStr, OsString};
+use std::collections::HashMap;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -10,22 +11,31 @@ struct Args {
     /// Target files
     #[arg(required = true)]
     files: Vec<PathBuf>,
+    // TODO:
+    // - ignore missing files
+}
+
+#[derive(Debug)]
+struct FileAttrs {
+    file_name: PathBuf,
+    attrs: HashMap<OsString, String>,
 }
 
 fn main() {
     let args = Args::parse();
 
-    for f in args.files {
-        let mut xattrs = xattr::list::<&OsStr>(f.as_ref()).unwrap().peekable();
-        if xattrs.peek().is_none() {
-            println!("no xattr set on {:?}", f);
-            return;
+    let results : Vec<FileAttrs> = args.files.into_iter().map(|f| {
+        let xattrs = xattr::list::<&OsStr>(f.as_ref()).unwrap();
+        FileAttrs {
+            file_name: f.clone(),
+            attrs: xattrs
+                .map(|attr| (
+                    attr.clone(),
+                    str::from_utf8(&xattr::get::<&OsStr, &OsStr>(f.as_ref(), attr.as_ref()).unwrap().unwrap()).unwrap().to_owned()
+                ))
+                .collect(),
         }
+    }).collect();
 
-        println!("Extended attributes:");
-        for attr in xattrs {
-            let val = xattr::get::<&OsStr, &OsStr>(f.as_ref(), attr.as_ref());
-            println!(" - {:?} := {:?}", attr, str::from_utf8(&val.unwrap().unwrap()).unwrap());
-        }
-    }
+    println!("{:?}", results);
 }
