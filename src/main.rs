77b5d76e2aaf6b48ee51c43c9f114a065f5e3ae7
Matt Kingston <mattkingston@protonmail.com>
2022-12-08 21:00:00 +0100

    Basic implementation of cli arg

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 use xattr;
+use std::string::String;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Target file name
+    #[arg(last=true)]
+    file_name: String,
+}
 
 fn main() {
-    let mut xattrs = xattr::list("./trash").unwrap().peekable();
+    let args = Args::parse();
+
+    let mut xattrs = xattr::list(args.file_name).unwrap().peekable();
     if xattrs.peek().is_none() {
         println!("no xattr set on root");
         return;
