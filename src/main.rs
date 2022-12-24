2c710fa0b2b6da3b97d9f5469c0c6495d65d98c5
Matt Kingston <mattkingston@protonmail.com>
2022-12-24 16:21:57 +0100

    Replace channel with futures::stream::FuturesOrdered

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::collections::HashMap;
 use std::ffi::OsStr;
 use std::path::PathBuf;
 use std::str;
-use tokio::sync::mpsc;
 use xattr;
-use rand::{thread_rng, Rng};
+use futures::stream::FuturesOrdered;
+use futures::StreamExt;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -45,40 +45,31 @@ fn build_attrs_for_file(file_name: &PathBuf) -> String {
 async fn main() {
     let args = Args::parse();
 
-    let (tx, mut rx) = mpsc::unbounded_channel::<String>();
+    let mut stream = FuturesOrdered::new();
 
     print!("[");
 
-    if args.files.len() > 0 {
-        // Extract a random item from the file list so we can print that after all the others, with
-        // no trailing comma. We remove a random item because the files will be returned
-        // out-of-order. We return files out-of-order because we process them concurrently and do
-        // not perform any sorting. This has the following advantages:
-        // - concurrency means the program should finish more quickly for a large number of files
-        // - not sorting means we can stream all output as it arrives, reducing memory usage
-        //   (though this *could* be mitigated by sorting on arrival and printing all items that
-        //   are printable- e.g. if items 1-3, 5 and 11 have arrived, we can print 1-3, etc.- this
-        //   will not be done at the time of writing..)
-        // - not sorting means we don't need to implement/maintain sorting code
-        let mut rng = thread_rng();
-        let mut rest = args.files.to_owned();
-        let last_file = rest.swap_remove(rng.gen_range(0..args.files.len()));
-
-        for f in rest {
-            let tx = tx.clone();
+    for f in args.files {
+        stream.push_back(
             tokio::spawn(async move {
-                tx.send(build_attrs_for_file(&f));
-            });
-        }
+                build_attrs_for_file(&f)
+            })
+        )
+    }
 
-        drop(tx); // Drop tx here so the receive channel closes when all the attributes have been received.
-        while let Some(s) = rx.recv().await {
-            // TODO: when does print flush? Should we ever flush manually? Expose this to the user?
-            print!("{},", s);
-        }
-
-        print!("{}", build_attrs_for_file(&last_file));
+    while let Some(attrs) = stream.next().await {
+        print!("{}", attrs.unwrap());
     }
 
     println!("]");
 }
+
+// Tests:
+// - we produce valid json with zero results
+// - we produce valid json with one results
+// - we produce valid json with two results
+// - we print output for files with zero attributes
+// - we print output for files with one attribute
+// - we print output for files with two attributes
+// - what do we do on missing files?
+// - what do we do on insufficient permissions?
